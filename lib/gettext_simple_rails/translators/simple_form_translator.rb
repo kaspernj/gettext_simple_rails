1596f72668a9a5c8f5a5bf7846f12d9540bb75a6
Kasper Johansen <k@spernj.org>
2014-05-18 22:50:20 +0200

    Added simple form hints to translations.

--- lib/gettext_simple_rails/translators/simple_form_translator.rb
+++ lib/gettext_simple_rails/translators/simple_form_translator.rb
@@ -4,6 +4,23 @@ class GettextSimpleRails::Translators::SimpleFormTranslator
   end
   
   def translations
-    return {}
+    @translations = {"simple_form" => {
+      "hints" => {}
+    }}
+    
+    hints = @translations["simple_form"]["hints"]
+    
+    GettextSimpleRails::ModelInspector.model_classes do |inspector|
+      clazz_snake_name = StringCases.camel_to_snake(inspector.clazz.name)
+      
+      clazz_hints = {}
+      hints[clazz_snake_name] = clazz_hints
+      
+      inspector.attributes do |attribute|
+        clazz_hints[attribute.name.to_s] = ""
+      end
+    end
+    
+    return @translations
   end
 end
