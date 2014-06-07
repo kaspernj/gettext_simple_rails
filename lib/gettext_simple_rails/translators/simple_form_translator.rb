514c8f7fa64a7be1bcbed22ed64b8b30a8148cbf
kaspernj <k@spernj.org>
2014-06-07 16:03:52 +0200

    Added placeholders.

--- lib/gettext_simple_rails/translators/simple_form_translator.rb
+++ lib/gettext_simple_rails/translators/simple_form_translator.rb
@@ -5,16 +5,19 @@ class GettextSimpleRails::Translators::SimpleFormTranslator
   
   def translations
     @translations = {"simple_form" => {
-      "hints" => {}
+      "hints" => {},
+      "placeholders" => {}
     }}
     
     hints = @translations["simple_form"]["hints"]
+    placeholders = @translations["simple_form"]["placeholders"]
     
     GettextSimpleRails::ModelInspector.model_classes do |inspector|
       clazz_snake_name = StringCases.camel_to_snake(inspector.clazz.name)
-      
       clazz_hints = {}
+      
       hints[clazz_snake_name] = clazz_hints
+      placeholders[clazz_snake_name] = clazz_hints
       
       inspector.attributes do |attribute|
         clazz_hints[attribute.name.to_s] = ""
