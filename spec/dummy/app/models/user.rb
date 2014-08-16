f2f77ac7a65716ab71f6cb525ae45ed1649a830d
kaspernj <k@spernj.org>
2014-08-16 14:55:14 +0200

    Fixed active record reflection depends error translations.

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -2,9 +2,9 @@ require "email_validator"
 
 class User < ActiveRecord::Base
   translates :title
-  
-  has_many :roles
-  
+
+  has_many :roles, dependent: :restrict_with_error
+
   validates :name, :length => {:in => 2..255}
   validates :name, :uniqueness => true, :presence => true
   validates :name, :format => {:with => /\A[A-z]+\Z/}
